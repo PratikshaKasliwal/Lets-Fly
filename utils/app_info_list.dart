cbe79d66b3e9c4db0bfe9b371edf0e28ddfc646b
PratikshaKasliwal <96160302+PratikshaKasliwal@users.noreply.github.com>
2023-09-25 19:52:03 +0530

    Add files via upload

---
+++ utils/app_info_list.dart
@@ -0,0 +1,51 @@
+List<Map<String, dynamic>> hotelList=[
+  {
+    'image':'Image_2.png',
+    'place':'open Space',
+    'destination':'London',
+    'price':25
+  },
+{
+    'image':'Image_3.png',
+    'place':'Global Will',
+    'destination':'London',
+    'price':40
+},
+  {
+    'image':'Image_4.png',
+    'place':'Tallest Building',
+    'destination':'Dubai',
+    'price':68
+  },
+];
+
+List<Map<String, dynamic>> ticketList=[
+  {
+    'from':{
+      'code': 'NYC',
+      'name':'New-York'
+    },
+    'to':{
+      'code':'LDN',
+      'name':'London'
+    },
+    'flying_time':'8H 30M',
+    'date':"1 MAY",
+    'departure_time':'08:00 AM',
+    'number':23
+  },
+  {
+    'from':{
+      'code': 'DK',
+      'name':'Dhaka'
+    },
+    'to':{
+      'code':'SH',
+      'name':'Shanghai'
+    },
+    'flying_time':'4H 20M',
+    'date':"10 MAY",
+    'departure_time':'09:00 AM',
+    'number':45
+  },
+];
